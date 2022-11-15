445555dd72e943577079f99907d6306cced044a7
DhirajLucidux <88828651+DhirajLucidux@users.noreply.github.com>
2022-11-15 15:54:09 +0530

    added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , redirectTo:'Photoedit', pathMatch:'full'},
+  {path:'Photoedit' , component:PhotoEditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
